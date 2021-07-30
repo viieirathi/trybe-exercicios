38dac33f3f30f5bd4a4ac575db225beb4e055315
Thiago <thiivieira@gmail.com>
2021-07-30 15:48:06 -0300

    exercicio conteudo

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/DIA-3/exerciciosc.js
@@ -0,0 +1,18 @@
+// let fruits = [3, 4, 10, 1, 12];
+// let soma = 0;
+
+// for (let index = 0; index < fruits.length; index += 1 ) {
+//   soma += fruits[index]
+// }
+
+
+// // for (let number of fruits) {
+// //   soma += fruits[index]
+// // }
+
+// if(fruits > 15){
+//   soma += fruits[index]
+//   console.log("O valor da array é maior", fruits);
+// } else {
+//   console.log("valor menor que 16 ");
+// }
